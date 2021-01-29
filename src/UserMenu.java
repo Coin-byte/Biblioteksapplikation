27797e0dc7ef4023fb97a156d0fc2e2e74e96289
Coin-byte <kriswern@icloud.com>
2021-01-29 11:18:58 +0100

    En grund för menyer

---
+++ src/UserMenu.java
@@ -0,0 +1,23 @@
+public enum UserMenu implements HasDescription{
+
+    //Descriptions är placeholders, behöver inte vara the final product
+    SHOW_ALL_BOOKS("Show all books in library."),
+    SHOW_AVAILABLE_BOOKS("Show available books"),
+    //Show books sorted by title
+    //Show books sorted by author
+    SHOW_DESCRIPTION("Show description"),
+    SEARCH_LIBRARY("Search library"),
+    SHOW_ALL_BORROWED("Show currently borrowed"), //And return date?
+    BORROW_NEW_BOOK("Borrow"),
+    RETURN_BOOK("Return");
+
+    private String description;
+
+    UserMenu(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
