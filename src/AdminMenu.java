f863d362a768717108cd16a10477a50d4499654c
haik91 <hadi33806@gmail.com>
2021-02-05 17:41:01 +0100

    DeleteUser() method and new alternatives were added to  admins menu

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -6,7 +6,9 @@ public enum AdminMenu implements HasDescription{
     DISPLAY_CURRENTLY_BORROWED("Show all borrowed books"),
     DISPLAY_USERS_AND_BOOKS("Display all users"),
     DISPLAY_USER_AND_BOOKS("Search for user"), //and display books user has
-    ADD_NEW_USER("Add new user");
+    DISPLAY_CERTAIN_USERS_BORROWED_BOOK("show borrowed books of a certain user "),
+    ADD_NEW_USER("Add new user"),
+    DELETE_A_USER("Delete user");
 
     private String description;
 
